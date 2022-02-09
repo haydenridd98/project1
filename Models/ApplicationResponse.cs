9cc7d9c094ab1281940837ab0dad1f95484174af
cadenkarras16 <caden.karras4@gmail.com>
2022-02-09 09:28:02 -0700

    Added in the Application Response
    Added in the application response real quick. Not totally complete but a good start.

---
+++ Models/ApplicationResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace project1.Models
+{
+    public class ApplicationResponse
+    {
+        //task(string), due date(date), quadrant(string), category(string), completed(bool)
+
+        public int TaskID { get; set; }
+
+        public string Task { get; set; }
+
+        [Required]
+        public string Quadrant { get; set; }
+
+        public string Category { get; set; }
+
+        public bool completed { get; set; }
+
+    }
+}
